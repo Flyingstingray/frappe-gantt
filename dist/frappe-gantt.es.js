2863589c55791bc3d9253e31b9ab3540d1fa2bb6
Gabrielle <gabrielle@stonemillconstruction.com>
2025-08-26 13:55:41 -0600

    Builds for prod

--- dist/frappe-gantt.es.js
+++ dist/frappe-gantt.es.js
@@ -594,14 +594,19 @@ class F {
   compute_x() {
     const { column_width: t } = this.gantt.config, e = this.task._start, i = this.gantt.gantt_start;
     let r = d.diff(e, i, this.gantt.config.unit) / this.gantt.config.step * t;
-    this.x = r;
+    this.x = Math.round(r);
   }
   compute_y() {
     this.y = this.gantt.config.header_height + this.gantt.options.padding / 2 + this.task._index * (this.height + this.gantt.options.padding);
   }
   compute_duration() {
     let t = 0, e = 0;
-    for (let i = new Date(this.task._start); i <= this.task._end; i.setDate(i.getDate() + 1))
+    for (
+      let i = new Date(this.task._start);
+      /*THIS IS CHANGED*/
+      i <= this.task._end;
+      i.setDate(i.getDate() + 1)
+    )
       e++, !this.gantt.config.ignored_dates.find(
         (s) => s.getTime() === i.getTime()
       ) && (!this.gantt.config.ignored_function || !this.gantt.config.ignored_function(i)) && t++;
